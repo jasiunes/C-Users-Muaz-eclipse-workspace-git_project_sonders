12dcfdb1d8cdbf0bf5be51b0b3c527214265beeb
yasemin <muazunes@gmail.com>
2020-08-16 22:07:42 +0200

    Feature

--- src/git_project_sonders/SonDerstEKRAR.java
+++ src/git_project_sonders/SonDerstEKRAR.java
@@ -3,5 +3,6 @@ package git_project_sonders;
 public class SonDerstEKRAR {
 public static void main(String[] args) {
 	System.out.println("21:55 -master branch");
+	System.out.println("22:05-feature branch");
 }
 }
